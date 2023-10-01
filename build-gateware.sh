dfe1d8ecf35c71a5a1914967fa6421a570510c52
Jan Schiefer <janschiefer@users.noreply.github.com>
2023-10-02 00:58:39 +0200

    Add LEDs, fix gpio! macro

--- build-gateware.sh
+++ build-gateware.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 source /mnt/Coding/litex/bin/activate
-/mnt/Coding/litex/litex-boards/litex_boards/targets/radiona_ulx3s_custom.py --build --doc --cpu-type vexriscv --cpu-variant imac --sys-clk-freq 50e6  --with-spi-sdcard --device LFE5U-85F --revision 2.0 --toolchain trellis --csr-svd build/ulx3s.svd --csr-json build/ulx3s.json
-#( cd litex-pac && svd2rust -i ../build/ulx3s.svd --target riscv && mv lib.rs src/ )
+rm -Rfd build/*
+rm -Rfd litex-pac_tmp/*
+/mnt/Coding/litex/litex-boards/litex_boards/targets/radiona_ulx3s_custom.py --build --doc --cpu-type vexriscv --cpu-variant imac --sys-clk-freq 25e6  --with-spi-sdcard --device LFE5U-85F --revision 2.0 --toolchain trellis --csr-svd build/ulx3s.svd --csr-json build/ulx3s.json
+( cd litex-pac_tmp && svd2rust -i ../build/ulx3s.svd --target riscv && mv lib.rs src/ )
 sphinx-build -M html build/radiona_ulx3s_custom/doc build/radiona_ulx3s_custom/_doc
--- litex-rusty-app/src/main.rs
+++ litex-rusty-app/src/main.rs
@@ -4,8 +4,7 @@
 use embedded_hal;
 use embedded_hal::blocking::delay::DelayMs;
 use embedded_hal::blocking::serial::Write;
-use embedded_hal::digital::v2::{OutputPin, ToggleableOutputPin};
-use nb;
+use embedded_hal::digital::v2::ToggleableOutputPin;
 use riscv;
 
 extern crate panic_halt;
@@ -13,8 +12,80 @@ use litex_hal as hal;
 use litex_pac as pac;
 use riscv_rt::entry;
 
+macro_rules! gpio {
+    ($(
+        $GPIOX:ident: $PACGPIOX:ty,
+    )+) => {
+        $(
+            #[derive(Debug)]
+            pub struct $GPIOX {
+                pub index: usize,
+            }
+
+            impl $GPIOX {
+                pub fn new(index: usize) -> Self {
+                    Self { index }
+                }
+            }
+
+            impl embedded_hal::digital::v2::OutputPin for $GPIOX {
+                type Error = core::convert::Infallible;
+
+                fn set_low(&mut self) -> Result<(), Self::Error> {
+                    let reg = unsafe { &*<$PACGPIOX>::ptr() };
+                    let mask: u32 = !(1 << self.index);
+                    riscv::interrupt::free(|| {
+                        let val: u32 = reg.out.read().bits() & mask;
+                        unsafe {
+                            reg.out.write(|w| w.bits(val));
+                        }
+                    });
+                    Ok(())
+                }
+                fn set_high(&mut self) -> Result<(), Self::Error> {
+                    let reg = unsafe { &*<$PACGPIOX>::ptr() };
+                    let mask: u32 = 1 << self.index;
+                    riscv::interrupt::free(|| {
+                        let val: u32 = reg.out.read().bits() | mask;
+                        unsafe {
+                            reg.out.write(|w| w.bits(val));
+                        }
+                    });
+                    Ok(())
+                }
+            }
+
+            impl embedded_hal::digital::v2::StatefulOutputPin for $GPIOX {
+                fn is_set_low(&self) -> Result<bool, Self::Error> {
+                    let reg = unsafe { &*<$PACGPIOX>::ptr() };
+                    let mask: u32 = 1 << self.index;
+                    let val: u32 = reg.out.read().bits() & mask;
+                    Ok(val == 0)
+                }
+                fn is_set_high(&self) -> Result<bool, Self::Error> {
+                    let reg = unsafe { &*<$PACGPIOX>::ptr() };
+                    let mask: u32 = 1 << self.index;
+                    let val: u32 = reg.out.read().bits() & mask;
+                    Ok(val != 0)
+                }
+            }
+
+            /// Opt-in to the software implementation.
+            impl embedded_hal::digital::v2::toggleable::Default for $GPIOX {}
+        )+
+    }
+}
+
 hal::uart! {
     UART: pac::UART,
+}
+
+hal::timer! {
+    TIMER: pac::TIMER0,
+}
+
+gpio! {
+    LEDS: pac::LEDS,
 }
 
 // This is the entry point for the application.
@@ -27,7 +98,18 @@ fn main() -> ! {
         registers: peripherals.UART,
     };
 
-    serial.bwrite_all(b"Hello world!\n").unwrap();
+    let mut delay = TIMER {
+        registers: peripherals.TIMER0,
+        sys_clk: 50_000_000,
+    };
 
-    loop {}
+    loop {
+        serial.bwrite_all(b"Hello world!\n").unwrap();
+
+        for i in 0..8 {
+            LEDS { index: i }.toggle().unwrap();
+        }
+
+        delay.delay_ms(1000 as u32);
+    }
 }
